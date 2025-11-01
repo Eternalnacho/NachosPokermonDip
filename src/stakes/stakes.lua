7587e7b4b2c2f41a022b8a71292d7fdc57776ebd
Eternalnacho <ahanshawbhaskar@gmail.com>
2025-11-01 13:34:30 -0500

    Elite Stake interactions with other Add-on Stakes

--- src/stakes/stakes.lua
+++ src/stakes/stakes.lua
@@ -1,8 +1,11 @@
 local elite = { -- Elite Stake
     key = 'elite_stake',
-    applied_stakes = {'gold'},
-    above_stake = 'gold',
-    prefix_config = {above_stake = {mod = false}, applied_stakes = {mod = false}},
+    applied_stakes = (SMODS.Mods["SonfivesPokermonPlus"] or {}).can_load and {'sonfive_lilac_stake'} or {'gold'},
+    above_stake = (SMODS.Mods["SonfivesPokermonPlus"] or {}).can_load and 'sonfive_lilac_stake' or 'gold',
+    prefix_config = {
+      above_stake = {mod = false},
+      applied_stakes = {mod = false}
+    },
 
     modifiers = function()
         G.GAME.win_ante = (G.GAME.win_ante + 2)
@@ -13,7 +16,7 @@ local elite = { -- Elite Stake
     sticker_pos = {x = 0, y = 0},
     atlas = 'stakes',
     sticker_atlas = 'stake_stickers',
-    colour = HEX("786104"),
+    colour = HEX("ba8514"),
     shiny = true
 }
 
