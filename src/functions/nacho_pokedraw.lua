8094fdbe7eaa3db88dcee82f3d6da63d40357dff
Eternalnacho <ahanshawbhaskar@gmail.com>
2026-03-03 22:02:18 -0600

    looping the stellar gradient color declarations

--- src/functions/nacho_pokedraw.lua
+++ src/functions/nacho_pokedraw.lua
@@ -32,49 +32,19 @@ SMODS.DrawStep({
 
 
 -- Rainbow Gradient for Stellar Type
+local stellar_colours = {HEX('cb4c44'), HEX('cc7b00'), HEX('c4af36'), HEX('43b645'), HEX('2ea4b6'), HEX('515fea'), HEX('9849d3'), HEX('cf3aa6')}
+for i = 1, #stellar_colours do
+  local index_cols = {}
+  for j = 1, #stellar_colours do table.insert(index_cols, stellar_colours[(i + j) % #stellar_colours]) end
+  SMODS.Gradient{
+    key = 'sg'..i,
+    colours = index_cols,
+    cycle = 5
+  }
+end
 
 SMODS.Gradient{
    key = 'sgbadge',
    colours = {G.C.RED, G.C.FILTER, HEX('f5db43'), HEX('54e456'), HEX('39cde4'), HEX('515fea'), HEX('a951ea'), HEX('e640b8')},
-   cycle = 5,
-}
-SMODS.Gradient{
-   key = 'sg1',
-   colours = {HEX('cb4c44'), HEX('cc7b00'), HEX('c4af36'), HEX('43b645'), HEX('2ea4b6'), HEX('515fea'), HEX('9849d3'), HEX('cf3aa6')},
-   cycle = 5,
-}
-SMODS.Gradient{
-   key = 'sg2',
-   colours = {HEX('cc7b00'), HEX('c4af36'), HEX('43b645'), HEX('2ea4b6'), HEX('515fea'), HEX('9849d3'), HEX('cf3aa6'), HEX('cb4c44')},
-   cycle = 5,
-}
-SMODS.Gradient{
-   key = 'sg3',
-   colours = {HEX('c4af36'), HEX('43b645'), HEX('2ea4b6'), HEX('515fea'), HEX('9849d3'), HEX('cf3aa6'), HEX('cb4c44'), HEX('cc7b00')},
-   cycle = 5,
-}
-SMODS.Gradient{
-   key = 'sg4',
-   colours = {HEX('43b645'), HEX('2ea4b6'), HEX('515fea'), HEX('9849d3'), HEX('cf3aa6'), HEX('cb4c44'), HEX('cc7b00'), HEX('c4af36')},
-   cycle = 5,
-}
-SMODS.Gradient{
-   key = 'sg5',
-   colours = {HEX('2ea4b6'), HEX('515fea'), HEX('9849d3'), HEX('cf3aa6'), HEX('cb4c44'), HEX('cc7b00'), HEX('c4af36'), HEX('43b645')},
-   cycle = 5,
-}
-SMODS.Gradient{
-   key = 'sg6',
-   colours = {HEX('515fea'), HEX('9849d3'), HEX('cf3aa6'), HEX('cb4c44'), HEX('cc7b00'), HEX('c4af36'), HEX('43b645'), HEX('2ea4b6')},
-   cycle = 5,
-}
-SMODS.Gradient{
-   key = 'sg7',
-   colours = {HEX('9849d3'), HEX('cf3aa6'), HEX('cb4c44'), HEX('cc7b00'), HEX('c4af36'), HEX('43b645'), HEX('2ea4b6'), HEX('515fea')},
-   cycle = 5,
-}
-SMODS.Gradient{
-   key = 'sg8',
-   colours = {HEX('cf3aa6'), HEX('cb4c44'), HEX('cc7b00'), HEX('c4af36'), HEX('43b645'), HEX('2ea4b6'), HEX('515fea'), HEX('9849d3')},
    cycle = 5,
 }
