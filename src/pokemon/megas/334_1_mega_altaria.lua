fd9089eb3fd52b028a39f06267cf6bfb1626e8f2
Eternalnacho <ahanshawbhaskar@gmail.com>
2026-04-08 15:51:31 -0500

    Mega Altaria effect change

--- src/pokemon/megas/334_1_mega_altaria.lua
+++ src/pokemon/megas/334_1_mega_altaria.lua
@@ -4,19 +4,16 @@ local mega_altaria = {
   nacho_inject_prefix = "poke",
   pos = { x = 2, y = 6 },
   soul_pos = { x = 3, y = 6 },
-  config = { extra = {chips = 0, threshold = 3, drawn = 0, money_mod = 2} },
+  config = { extra = {chips = 0, money_mod = 2} },
   loc_txt = {
     name = "Mega Altaria",
     text = {
       "{C:chips}+#1#{} Chips,",
       "{C:attention}9s can't{} be debuffed",
       "{br:2.5}ERROR - CONTACT STEAK",
-      "Every third {C:inactive}#2#{C:attention}9{}",
-      "drawn during the {C:attention}Blind{}",
-      "becomes {C:dark_edition}Polychrome{}",
-      "{br:2.5}ERROR - CONTACT STEAK",
-      "{C:attention}Editioned{} cards drawn",
-      "during the {C:attention}Blind{} earn {C:money}$#3#{}",
+      "{C:attention}9s{} drawn during the",
+      "{C:attention}Blind{} become {C:dark_edition}Polychrome{}",
+      "and earn {C:money}$#2#{}",
     }
   },
   loc_vars = function(self, info_queue, center)
@@ -26,8 +23,7 @@ local mega_altaria = {
         info_queue[#info_queue+1] = G.P_CENTERS.e_polychrome
       end
     end
-    local draw_count = G.playing_cards and G.STAGE == G.STAGES.RUN and '['..center.ability.extra.drawn..'/'..center.ability.extra.threshold..'] ' or ''
-    return {vars = {center.ability.extra.chips, draw_count, center.ability.extra.money_mod}}
+    return {vars = {center.ability.extra.chips, center.ability.extra.money_mod}}
   end,
   rarity = "poke_mega",
   cost = 12,
@@ -41,7 +37,13 @@ local mega_altaria = {
     -- Convert every third 9 drawn to polychrome
     if context.hand_drawn and SMODS.drawn_cards and not context.blueprint then
       PkmnDip.utils.for_each(SMODS.drawn_cards, function(pcard)
-        if pcard.edition then
+        if pcard:get_id() == 9 then
+          -- Make drawn 9s polychrome
+          G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
+            pcard:set_edition('e_polychrome', true)
+          return true end }))
+
+          -- and earn $2
           local earned = ease_poke_dollars(card, "mega_altaria", card.ability.extra.money_mod)
           G.GAME.dollar_buffer = (G.GAME.dollar_buffer or 0) + earned
           G.E_MANAGER:add_event(Event({
@@ -50,16 +52,6 @@ local mega_altaria = {
                   return true
               end
           }))
-        end
-
-        if pcard:get_id() == 9 then
-          card.ability.extra.drawn = card.ability.extra.drawn + 1
-          if card.ability.extra.drawn == card.ability.extra.threshold then
-            G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
-              pcard:set_edition('e_polychrome', true)
-            return true end }))
-            card.ability.extra.drawn = 0
-          end
         end
       end)
     end
