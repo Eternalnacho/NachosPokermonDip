41b24d237bb7fccb11480a0416169092858f21da
Eternalnacho <ahanshawbhaskar@gmail.com>
2026-05-17 22:07:35 -0500

    chimchar adjustment

--- src/pokemon/0390_chimchar.lua
+++ src/pokemon/0390_chimchar.lua
@@ -33,7 +33,7 @@ local chimchar={
 
     if context.end_of_round and context.main_eval and not context.blueprint then
       extra.mult = 0
-      card_eval_status_text( card, 'extra', nil, nil, nil, { message = localize('k_reset'), colour = G.C.RED } )
+      SMODS.calculate_effect({message = localize('k_reset')}, card)
     end
 
     return level_evo(self, card, context, "j_nacho_monferno")
@@ -80,7 +80,7 @@ local monferno={
 
     if context.end_of_round and context.game_over == false and context.main_eval and not context.blueprint then
       extra.mult = 0
-      card_eval_status_text( card, 'extra', nil, nil, nil, { message = localize('k_reset'), colour = G.C.RED } )
+      SMODS.calculate_effect({message = localize('k_reset')}, card)
     end
 
     return level_evo(self, card, context, "j_nacho_infernape")
@@ -99,7 +99,7 @@ local monferno={
 -- Infernape 392
 local infernape = {
   name = "infernape",
-  config = { extra = { d_size = 1, mult = 30, Xmult = 1.0, Xmult_mod = 0.3 } },
+  config = { extra = { d_size = 1, mult = 30, Xmult = 1, Xmult1 = 1, Xmult_mod = 0.3 } },
   loc_vars = function(self, info_queue, card)
     local extra = card.ability.extra
     type_tooltip(self, info_queue, card)
@@ -113,15 +113,18 @@ local infernape = {
   blueprint_compat = true,
   eternal_compat = true,
   calculate = function(self, card, context)
-    local extra = card.ability.extra
-
+    -- Discarding Face cards or Aces scales Infernape
     if context.discard and not context.blueprint and not context.other_card.debuff then
       if context.other_card:is_face() or context.other_card:get_id() == 14 then
-        extra.Xmult = extra.Xmult + extra.Xmult_mod
-        return { message = localize('k_upgrade_ex'), colour = G.C.RED }
+        SMODS.scale_card(card, {
+          ref_value = 'Xmult',
+          scalar_value = 'Xmult_mod',
+          message_key = 'a_xmult',
+          message_colour = G.C.XMULT,
+        })
       end
     end
-
+    -- Main Scoring
     if context.joker_main then
       return {
         message = localize("poke_close_combat_ex"),
@@ -130,10 +133,10 @@ local infernape = {
         Xmult_mod = card.ability.extra.Xmult,
       }
     end
-
+    -- Resets at end of round
     if context.end_of_round and context.main_eval and not context.blueprint then
-      card.ability.extra.Xmult = 1.0
-      return { message = localize('k_reset'), colour = G.C.RED }
+      card.ability.extra.Xmult = card.ability.extra.Xmult1
+      SMODS.calculate_effect({message = localize('k_reset')}, card)
     end
   end,
   add_to_deck = function(self, card, from_debuff)
