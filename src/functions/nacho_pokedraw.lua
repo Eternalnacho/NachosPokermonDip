82c5119cd8eb72bb994a56dc6a28857f8e150fa1
Eternalnacho <ahanshawbhaskar@gmail.com>
2026-05-20 13:12:26 -0500

    Hisuian Zorua  update but for real this time

--- src/functions/nacho_pokedraw.lua
+++ src/functions/nacho_pokedraw.lua
@@ -1,33 +1,24 @@
 SMODS.Shader({ key = 'hisuian_zorua', path = 'hisuian_zorua.fs' }):register()
 
 SMODS.DrawStep({
-   key = 'hisuian_zorua_shadow',
-   order = 69,
-   func = function(card, layer)
-      if not card or not card.ability or not card.children.center or (card.ability.name ~= 'hisuian_zorua' and card.ability.name ~= 'hisuian_zoroark') then return end
-      if card.debuff or (card.ability.name == 'hisuian_zorua' and not card.ability.extra.active) then return end
-      if G.jokers and card.area == G.jokers then
-         local other_joker = G.jokers.cards[1]
-         if other_joker == card or other_joker.debuff or not other_joker.config.center.blueprint_compat then return end
-      end
-      local center = card.config.center
-      local prev_atlas = card.children.center.atlas
-      local prev_pos = card.children.center.sprite_pos
-      local new_atlas = (card.edition and card.edition.poke_shiny) and "poke_AtlasJokersBasicGen05Shiny" or "poke_AtlasJokersBasicGen05"
+  key = 'hisuian_zorua_shadow',
+  order = 69,
+  func = function(card, layer)
+    if card.debuff or poke_is_in_collection(card) or not card.ability then return end
+    if not ((card.config.center.key == 'j_nacho_hisuian_zorua' and card.ability.extra.active) or card.config.center.key == 'j_nacho_hisuian_zoroark') then return end
+    if card.area and card.area == G.jokers then
+      local other_joker = G.jokers.cards[1]
+      if other_joker == card or not other_joker.config.center.blueprint_compat then return end
+    end
 
-      if card.ability.received_card then
-        new_atlas = (card.edition and card.edition.poke_shiny) and "poke_AtlasJokersBasicNatdexShiny" or "poke_AtlasJokersBasicNatdex"
-      end
+    local center = card.ability.received_card or card.config.center
+    local dip_shared_key = 'shared_' .. card.ability.name
 
-      card.children.center.atlas = SMODS.get_atlas(new_atlas)
-      card.children.center:set_sprite_pos(center.pos)
-
-      card.children.center:draw_shader('nacho_hisuian_zorua', nil, card.ARGS.send_to_shader)
-
-      card.children.center.atlas = prev_atlas
-      card.children.center:set_sprite_pos(prev_pos)
-   end,
-   conditions = { vortex = false, facing = 'front' },
+    G[dip_shared_key] = G[dip_shared_key] or SMODS.create_sprite(0, 0, G.CARD_W, G.CARD_H, center.atlas, center.pos)
+    G[dip_shared_key].role.draw_major = card
+    G[dip_shared_key]:draw_shader('nacho_hisuian_zorua', nil, card.ARGS.send_to_shader, nil, card.children.center)
+  end,
+  conditions = { vortex = false, facing = 'front' },
 })
 
 
--- src/pokemon/0570_1_hisuian_zorua.lua
+++ src/pokemon/0570_1_hisuian_zorua.lua
@@ -1,55 +1,17 @@
--- ILLUSION-RELATED FUNCTIONS
-local function draw_illusion(card, other_joker)
-  -- Set sprite atlas + position to other joker's
-  card.children.center.atlas = other_joker.children.center.atlas
-  card.children.center:set_sprite_pos(other_joker.children.center.sprite_pos)
-  -- do the same for floating sprites if they exist
-  if other_joker.children.floating_sprite then
-    card.children.floating_sprite.atlas = other_joker.children.floating_sprite.atlas
-    card.children.floating_sprite:set_sprite_pos(other_joker.children.floating_sprite.sprite_pos)
-  else
-    card.children.floating_sprite.atlas = SMODS.get_atlas(card.config.center.atlas)
-    card.children.floating_sprite:set_sprite_pos(card.config.center.soul_pos)
-  end
-  -- save other joker's center key to loosely track position
-  card.ability.blueprint_joker = other_joker.config.center_key
-end
-
-local function remove_illusion(card)
-  card.children.floating_sprite:remove()
-  -- Reset atlas + position
-  card.children.center.atlas = G.ASSET_ATLAS["poke_AtlasJokersBasicGen05"..(card.edition and card.edition.poke_shiny and "Shiny" or "")]
-  card.children.center:set_sprite_pos(card.config.center.pos)
-  -- Reset floating sprite atlas + position
-  card.children.floating_sprite.atlas = G.ASSET_ATLAS["poke_AtlasJokersBasicGen05"..(card.edition and card.edition.poke_shiny and "Shiny" or "")]
-  card.children.floating_sprite:set_sprite_pos(card.config.center.soul_pos)
-  -- Stop tracking targeted joker
-  card.ability.blueprint_joker = nil
-end
-
--- Illusion UIBox
-local function generate_illusion_ui(other_center, info_queue, card, desc_nodes, specific_vars, full_UI_table)
-  -- Create the illusion joker's text boxes
-  local temp_ability = copy_table(card.ability) -- temp_ability needed so we can use the illusion's config values during generate_ui
-  for k, v in pairs(other_center.config) do card.ability[k] = v end
-  other_center:generate_ui(info_queue, card, desc_nodes, specific_vars, full_UI_table)
-  card.ability = temp_ability
-  -- Adds the "...?" to the end of the name
-  if next(full_UI_table.name[1].nodes) then
-    local UIname_nodes = full_UI_table.name[1].nodes
-    local textDyna = UIname_nodes[#UIname_nodes].nodes[1].config.object
-    textDyna:replace_text(textDyna.string .. localize("poke_illusion"))
-  end
-end
-
-
--- ACTUAL JOKERS
 -- Hisuian Zorua 570-1
 local hisuian_zorua = {
   name = "hisuian_zorua",
   pos = { x = 0, y = 9 },
   soul_pos = { x = 99, y = 99 },
   config = {extra = {hidden_key = nil, rounds = 5, active = true}},
+  loc_vars = function(self, info_queue, card)
+    local main_end
+    if card.area and card.area == G.jokers then
+      local other_joker = G.jokers.cards[#G.jokers.cards]
+      main_end = poke_blueprint_compat_ui(card ~= other_joker and other_joker)
+    end
+    return {vars = {card.ability.extra.rounds, colours = {not card.ability.extra.active and G.C.UI.TEXT_INACTIVE}}, main_end = main_end}
+  end,
   designer = "ESN64",
   rarity = 3,
   cost = 8,
@@ -58,67 +20,73 @@ local hisuian_zorua = {
   gen = 5,
   blueprint_compat = true,
   rental_compat = false,
-  loc_vars = function(self, info_queue, card)
-    return {vars = {card.ability.extra.rounds, colours = {not card.ability.extra.active and G.C.UI.TEXT_INACTIVE}}}
+  get_illusion = function(self, card)
+    if card.ability and card.ability.extra
+        and card.area ~= G.jokers
+        and not poke_is_in_collection(card) then
+      return G.P_CENTERS[card.ability.extra.hidden_key]
+    end
   end,
   calculate = function(self, card, context)
     local a, other_joker = card.ability.extra, G.jokers.cards[1]
-    if other_joker and other_joker ~= card and a.active then
+    if a.active and other_joker and other_joker ~= card then
       local ret = SMODS.blueprint_effect(card, other_joker, context)
       if ret then
         ret.colour = G.C.BLACK
         return ret
       end
     end
-    if context.after and G.jokers.cards[1] ~= card and a.active then
+    if context.after then
       PkmnDip.defer(function()
         a.active = false
         SMODS.calculate_effect({message = localize('poke_reveal_ex')}, card)
       end, 0.2)
     end
-    if context.end_of_round and context.game_over == false and context.main_eval then
+    if context.end_of_round and context.game_over == false and context.main_eval and not a.active then
       a.active = true
-      SMODS.calculate_effect({message = localize('k_reset')}, card)
+      return { message = localize('k_reset') }
     end
     return level_evo(self, card, context, "j_nacho_hisuian_zoroark")
   end,
+  set_card_type_badge = function(self, card, badges)
+    poke_set_card_type_badge(card, badges, self:get_illusion(card))
+  end,
   set_sprites = function(self, card, front)
-    if card.ability and card.ability.extra and card.ability.extra.hidden_key then
-      local center = G.P_CENTERS[card.ability.extra.hidden_key]
+    local center = self:get_illusion(card)
+    if center then
       card:set_sprites(center)
     end
   end,
   set_ability = function(self, card, initial, delay_sprites)
     if card.area ~= G.jokers and not poke_is_in_collection(card) then
+      -- Initialize the Illusion
       if not type_sticker_applied(card) then apply_type_sticker(card, "Colorless") end
-      card.ability.extra.hidden_key = card.ability.extra.hidden_key or get_random_poke_key('zorua', nil, 1)
+      card.ability.extra.hidden_key = card.ability.extra.hidden_key or get_random_poke_key_options {
+        key_append = 'zorua_h',
+        rarity = 'Common',
+        exclude_types = 'Colorless',
+      }
       self:set_sprites(card)
     end
   end,
   generate_ui = function(self, info_queue, card, desc_nodes, specific_vars, full_UI_table)
-    if card.area ~= G.jokers and not poke_is_in_collection(card) then
-      local center = G.P_CENTERS[card.ability.extra.hidden_key]
-      if center then
-        return generate_illusion_ui(center, info_queue, card, desc_nodes, specific_vars, full_UI_table)
-      end
+    local center = self:get_illusion(card)
+    if center then
+      return poke_generate_illusion_ui(center, info_queue, card, desc_nodes, specific_vars, full_UI_table)
     end
     return SMODS.Center.generate_ui(self, info_queue, card, desc_nodes, specific_vars, full_UI_table)
   end,
   update = function(self, card, dt)
-    if G.STAGE == G.STAGES.RUN and card.area == G.jokers then
-      if card.ability.extra.hidden_key then card.ability.extra.hidden_key = nil end
+    if G.STAGE == G.STAGES.RUN and card.area and card.area == G.jokers then
       local other_joker = G.jokers.cards[1]
-      card.ability.blueprint_compat = ( other_joker and other_joker ~= card and not other_joker.debuff
-        and other_joker.config.center.blueprint_compat ) and 'compatible' or 'incompatible'
-      if card.ability.blueprint_compat == 'compatible' and not card.debuff and card.ability.extra.active then
-        if other_joker and card.ability.blueprint_joker ~= other_joker.config.center_key then
-          draw_illusion(card, other_joker)
-        end
+      if card.ability.extra.active and not card.debuff
+          and other_joker and other_joker ~= card
+          -- and other_joker.children.center.atlas.px == 71 -- Disables Unown Swarm drawing, because I just couldn't be bothered today.
+          and other_joker.config.center.blueprint_compat then
+        poke_copy_joker_sprites(card, other_joker)
       else
-        remove_illusion(card)
+        poke_reset_sprite(card)
       end
-    elseif poke_is_in_collection(card) and card.children.center.sprite_pos ~= self.pos and card.children.center.atlas.name ~= self.atlas then
-      self:set_ability(card)
     end
   end,
   attributes = {"copying", "round_evo"},
@@ -130,6 +98,14 @@ local hisuian_zoroark = {
   pos = { x = 2, y = 9 },
   soul_pos = { x = 99, y = 99 },
   config = {extra = {hidden_key = nil}},
+  loc_vars = function(self, info_queue, card)
+    local main_end
+    if card.area and card.area == G.jokers then
+      local other_joker = G.jokers.cards[1]
+      main_end = poke_blueprint_compat_ui(card ~= other_joker and other_joker)
+    end
+    return {main_end = main_end}
+  end,
   designer = "ESN64",
   rarity = "poke_safari",
   cost = 12,
@@ -137,6 +113,13 @@ local hisuian_zoroark = {
   ptype = "Colorless",
   gen = 5,
   blueprint_compat = true,
+  get_illusion = function(self, card)
+    if card.ability and card.ability.extra
+        and card.area ~= G.jokers
+        and not poke_is_in_collection(card) then
+      return G.P_CENTERS[card.ability.extra.hidden_key]
+    end
+  end,
   calculate = function(self, card, context)
     if G.jokers.cards[1] and G.jokers.cards[1] ~= card then
       local ret = SMODS.blueprint_effect(card, G.jokers.cards[1], context)
@@ -147,41 +130,47 @@ local hisuian_zoroark = {
     end
   end,
   set_sprites = function(self, card, front)
-    if card.ability and card.ability.extra and card.ability.extra.hidden_key then
-      local center = G.P_CENTERS[card.ability.extra.hidden_key]
+    local center = self:get_illusion(card)
+    if center then
       card:set_sprites(center)
     end
   end,
   set_ability = function(self, card, initial, delay_sprites)
     if card.area ~= G.jokers and not poke_is_in_collection(card) then
+      -- Initialize the Illusion
       if not type_sticker_applied(card) then apply_type_sticker(card, "Colorless") end
-      local args = {'zoroark', nil, 'poke_safari', nil, nil, {j_poke_zoroark = true, j_nacho_hisuian_zoroark = true}}
-      card.ability.extra.hidden_key = card.ability.extra.hidden_key or get_random_poke_key(table.unpack(args))
+      if not card.ability.extra.hidden_key then
+        card.ability.extra.hidden_key = get_random_poke_key_options {
+          key_append = 'zoroark_h',
+          rarity = 'poke_safari',
+          exclude_types = 'Colorless',
+        }
+      end
       self:set_sprites(card)
     end
   end,
+  add_to_deck = function(self, card, from_debuff)
+    card.ability.extra.hidden_key = nil
+    poke_reset_sprite(card)
+  end,
   generate_ui = function(self, info_queue, card, desc_nodes, specific_vars, full_UI_table)
-    if card.area ~= G.jokers and not poke_is_in_collection(card) then
-      local center = G.P_CENTERS[card.ability.extra.hidden_key]
-      if center then
-        return generate_illusion_ui(center, info_queue, card, desc_nodes, specific_vars, full_UI_table)
-      end
+    local center = self:get_illusion(card)
+    if center then
+      return poke_generate_illusion_ui(center, info_queue, card, desc_nodes, specific_vars, full_UI_table)
     end
     return SMODS.Center.generate_ui(self, info_queue, card, desc_nodes, specific_vars, full_UI_table)
   end,
   update = function(self, card, dt)
-    if G.STAGE == G.STAGES.RUN and card.area == G.jokers then
-      if card.ability.extra.hidden_key then card.ability.extra.hidden_key = nil end
+    if G.STAGE == G.STAGES.RUN and card.area and card.area == G.jokers then
       local other_joker = G.jokers.cards[1]
-      card.ability.blueprint_compat = ( other_joker and other_joker ~= card and not other_joker.debuff
-        and other_joker.config.center.blueprint_compat ) and 'compatible' or 'incompatible'
-      if card.ability.blueprint_compat == 'compatible' and not card.debuff and other_joker and card.ability.blueprint_joker ~= other_joker.config.center_key then
-        draw_illusion(card, other_joker)
+      if not card.debuff
+          and other_joker and other_joker ~= card
+          -- and other_joker.children.center.atlas.px == 71 -- Disables Unown Swarm drawing, because I just couldn't be bothered today.
+          and other_joker.config.center.blueprint_compat then
+        poke_copy_joker_sprites(card, other_joker)
       else
-        remove_illusion(card)
+        poke_reset_sprite(card)
       end
-    elseif poke_is_in_collection(card) and card.children.center.sprite_pos ~= self.pos and card.children.center.atlas.name ~= self.atlas then
-      self:set_ability(card)
     end
   end,
   attributes = {"copying"},
