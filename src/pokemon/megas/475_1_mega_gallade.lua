09357fb673574f25b7f021de59ed7c9a21fc1828
Eternalnacho <ahanshawbhaskar@gmail.com>
2026-06-12 19:47:37 -0500

    mega gallade should in fact score

--- src/pokemon/megas/475_1_mega_gallade.lua
+++ src/pokemon/megas/475_1_mega_gallade.lua
@@ -33,6 +33,9 @@ local mega_gallade = {
       local target = pseudorandom_element(G.jokers.cards, 'mega_gallade')
       target.ability.extra.e_limit_up = target.ability.extra.e_limit_up and target.ability.extra.e_limit_up + 1 or 1
     end
+    if context.joker_main then
+      return { xmult = card.ability.extra.Xmult }
+    end
   end,
 }
 
